f60f3396d86e5e965090489de7b281db1baa8189
dbusrepo <dbusrepo@gmail.com>
2023-07-02 22:24:20 +0200

    Fix name case asc dir

--- src/engine/wasmEngine/wasm/src/asc/raycaster/raycaster.ts
+++ src/engine/wasmEngine/wasm/src/asc/raycaster/raycaster.ts
@@ -1,25 +1,22 @@
 import { myAssert } from '../myAssert';
+import { logi } from '../importVars';
 import { PTR_T, SIZE_T, NULL_PTR } from '../memUtils';
 import { ObjectAllocator, newObjectAllocator } from '../objectAllocator';
-import { logi } from '../importVars';
+import { SArray, newSArray } from '../sarray';
 import { Viewport, newViewport } from './viewport';
 import { Player, newPlayer } from './player';
-import { SArray, newSArray } from '../sarray';
+import { Map, newMap } from './map';
 
 @final @unmanaged class Raycaster {
-  public borderColor: u32;
-  public viewport: Viewport;
-  public player: Player;
-  public zBuffer: SArray<f32>;
+  private borderColor: u32;
+  private viewport: Viewport;
+  private player: Player;
+  private zBuffer: SArray<f32>;
+  private map: Map;
 
   postInit(): void {
     this.zBuffer = newSArray<f32>(this.Viewport.Width);
   }
-
-  // constructor() {
-  //   this.viewport = newViewport();
-  //   this.player = newPlayer();
-  // }
 
   get Viewport(): Viewport {
     return this.viewport;
@@ -37,16 +34,24 @@ import { SArray, newSArray } from '../sarray';
     this.player = player;
   }
 
+  get Map(): Map {
+    return this.map;
+  }
+
+  set Map(map: Map) {
+    this.map = map;
+  }
+
+  get ZBuffer(): SArray<f32> {
+    return this.zBuffer;
+  }
+
   get ViewportPtr(): PTR_T {
     return changetype<PTR_T>(this.viewport);
   }
 
   get PlayerPtr(): PTR_T {
     return changetype<PTR_T>(this.player);
-  }
-
-  get ZBufferPtr(): SArray<f32> {
-    return this.zBuffer;
   }
 }
 
@@ -70,7 +75,7 @@ function getRaycasterBorderColorOffset(basePtr: PTR_T): SIZE_T {
 
 function getRaycasterZBufferPtr(basePtr: PTR_T): SIZE_T {
   const raycaster = changetype<Raycaster>(basePtr);
-  return raycaster.zBuffer.DataPtr;
+  return raycaster.ZBuffer.DataPtr;
 }
 
 export {
